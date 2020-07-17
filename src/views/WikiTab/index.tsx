f5a262033c4a8b908a5fc469171862f55f62abc9
lucy-loo <thelucyloo70@gmail.com>
2020-07-18 03:44:14 +0800

    [update] update WikiTab module

--- src/views/WikiTab/index.tsx
+++ src/views/WikiTab/index.tsx
@@ -1,8 +1,15 @@
-import * as React from 'react'
-import * as globalStyle from '@/styles/index.module.css'
+import React from 'react'
+import WhiteSpace from '@/components/WhiteSpace'
+import localStyle from './styles/index.module.css'
+import MapTap from '../MapBox/MapTap'
 
 function WikiTab(): JSX.Element {
-  return <div className={globalStyle.wikiTab} />
+  return (
+    <div className={localStyle.wikiWrapper}>
+      <WhiteSpace />
+      <MapTap>疾病知识</MapTap>
+    </div>
+  )
 }
 
 export default WikiTab
