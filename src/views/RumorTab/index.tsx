71cfdc80cace6009ab619af4eb5989136165b491
lucy-loo <thelucyloo70@gmail.com>
2020-07-18 03:43:51 +0800

    [update] update RumorTab module

--- src/views/RumorTab/index.tsx
+++ src/views/RumorTab/index.tsx
@@ -1,8 +1,15 @@
 import * as React from 'react'
 import * as globalStyle from '@/styles/index.module.css'
+import WhiteSpace from '@/components/WhiteSpace'
+import MapTap from '../MapBox/MapTap'
 
 function RumorTab(): JSX.Element {
-  return <div className={globalStyle.rumerTabWrap} />
+  return (
+    <div className={globalStyle.rumerTabWrap}>
+      <WhiteSpace />
+      <MapTap>辟谣与防护</MapTap>
+    </div>
+  )
 }
 
 export default RumorTab
