f530c06ead1d444bec65c3b9b792c4c02a874513
lucy-loo <thelucyloo70@gmail.com>
2020-07-18 03:43:09 +0800

    [update] update RealTimeAlert module

--- src/views/RealTimeAlert/index.tsx
+++ src/views/RealTimeAlert/index.tsx
@@ -1,11 +1,13 @@
-import * as React from 'react'
-import * as globalStyle from '@/styles/index.module.css'
+import React from 'react'
+import globalStyle from '@/styles/index.module.css'
+import WhiteSpace from '@/components/WhiteSpace'
+import MapTap from '../MapBox/MapTap'
 
 function RealTimeAlert(): JSX.Element {
   return (
     <div className={globalStyle.realtime}>
-      <div className={globalStyle.whiteSpace} />
-      <p className={globalStyle.mapTap}>实时播报</p>
+      <WhiteSpace />
+      <MapTap>实时播报</MapTap>
       <Tab />
       <WatchMore />
     </div>
