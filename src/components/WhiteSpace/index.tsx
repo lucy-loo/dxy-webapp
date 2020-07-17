7b3d32dd6c3fefd92af75145a13e8003d371e17b
lucy-loo <thelucyloo70@gmail.com>
2020-07-18 03:42:00 +0800

    [update] add WhiteSpace component

---
+++ src/components/WhiteSpace/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import localStyle from './whitespace.module.css'
+
+function WhiteSpace() {
+  return <div className={localStyle.whiteSpace} />
+}
+export default WhiteSpace
